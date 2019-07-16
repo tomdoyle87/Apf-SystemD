c70ad8914bba3a3e39d075c1e323c8806c2e991b
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2019-07-16 12:12:31 +0100

    Stopping apf before uninstall

--- files/uninstall.sh
+++ files/uninstall.sh
@@ -2,6 +2,7 @@
 INSTALL_PATH=${INSTALL_PATH:-"/etc/apf"}
 if [ -d "/lib/systemd/system" ]; then
 	/bin/systemctl disable apf.service
+	/bin/systemctl stop apf.service
 	/bin/systemctl disable apf-daily.timer
 	/bin/systemctl stop apf-daily.timer
 	/bin/systemctl disable apf-daily.service
@@ -11,11 +12,13 @@ if [ -d "/lib/systemd/system" ]; then
 	rm -f {/lib/systemd/system/apf.service,/lib/systemd/system/apf-daily.service,/lib/systemd/system/apf-daily.target,/lib/systemd/system/apf-daily.timer,/lib/systemd/system/update-apf.service,/lib/systemd/system/update-apf.timer,/lib/systemd/system/update-apf.target,/usr/local/sbin/apf-restart.sh}
 elif [ -d "/etc/rc.d/init.d" ]; then
 	chkconfig apf off
+	/etc/rc.d/init.d/apf stop
 	rm -f /etc/rc.d/init.d/apf
 	rm -f /etc/cron.daily/apf
 	rm -f /etc/cron.weekly/apf
 elif [ -d "/etc/init.d" ]; then
 	chkconfig apf off
+	/etc/init.d/apf stop
 	rm -f /etc/init.d/apf
 	rm -f /etc/cron.daily/apf
 	rm -f /etc/cron.weekly/apf
